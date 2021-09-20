7bb34a965d049ab5d9df5e297c195c3882b1fd4b
ravulabhargavi <ravulabhargavi70@gmail.com>
2021-09-20 16:10:28 +0530

    commit BookingService

---
+++ src/test/java/com/htc/bookingservice/BookingServiceApplicationTests.java
@@ -0,0 +1,38 @@
+package com.htc.bookingservice;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import com.htc.bookingservice.entity.BookingDetails;
+import com.htc.bookingservice.persistance.BookingServiceInterface;
+
+
+
+@SpringBootTest
+class BookingServiceApplicationTests {
+	@Autowired
+	BookingServiceInterface repo;
+
+	@Test
+	void contextLoads() {
+	}
+	@Test
+	public void bookingdeatils_persistedInDataBase_elseTestFailure() {
+		BookingDetails bookingDetails = new BookingDetails();
+		bookingDetails.setBookingId(1L);
+	    bookingDetails.setCentreId(2L);
+	    bookingDetails.setPinCode("12345");
+		
+		repo.save(bookingDetails);
+		assertNotNull(repo.findById(1L).get());
+		
+	}
+
+
+}
